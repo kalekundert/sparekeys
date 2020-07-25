299c026700dab80f803a745ddf50f110a6f41a17
Kale Kundert <kale@thekunderts.net>
2020-07-25 16:27:01 -0400

    docs: add brief description of package

--- sparekeys/__init__.py
+++ sparekeys/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""\
+Make and distribute encrypted copies of the files that you would need to 
+recover from a catastrophic hard drive failure.
+"""
+
 from .main import *
 
 __version__ = '0.2.0'
